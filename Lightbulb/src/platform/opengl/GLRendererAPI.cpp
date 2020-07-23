c72e07e0a7e108dbfc7bcef2c16626b8ad49fe0c
Datasound9087 <samlharper133@gmail.com>
2020-07-23 23:14:46 +0100

    Enabled GL culling of back faces.

--- Lightbulb/src/platform/opengl/GLRendererAPI.cpp
+++ Lightbulb/src/platform/opengl/GLRendererAPI.cpp
@@ -41,6 +41,8 @@ void GLRendererAPI::init()
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_CULL_FACE);
 }
 
 void GLRendererAPI::setViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
